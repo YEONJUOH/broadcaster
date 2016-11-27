72504274e44baea30b7c37719a9e080070b75de1
YEONJUOH <eorol2@naver.com>
2016-11-27 17:19:17 +0900

    # 입찰란 수정

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../public/javascripts/pool.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/broadcast',function (req,res,next) {
-  console.log(req.body['b_key']);
-  res.render('broadcastPage',{'key':req.body['b_key']})
-})
+
+  var b_id = req.body['b_id'];
+  var m_id = req.body['m_id'];
+
+  pool.getConnection(function (err,con) {
+
+    con.query('select * from broadcast where b_id =? ', b_id ,function (err,result) {
+
+      if(!err) {
+        var broadcast = result[0];
+        con.query('select * from member where m_id =? ',m_id,function (err,result){
+          if(!err){
+            var member = result[0];
+            res.render('broadcastPage',{'broadcast': broadcast, "member": member });
+          }else{
+            console.log("2"+err);
+          };
+        });
+      }else{
+          console.log("1"+err);
+      }
+      con.release();
+    });
+  });
+
+});
+
 module.exports = router;
